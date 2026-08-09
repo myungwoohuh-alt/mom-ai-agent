b25136e099e783d41da3410003839d9e5591592e
myungwoohuh-alt <myungwoohuh@gmail.com>
2026-08-09 16:31:14 +0900

    Add  question response conditions

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ while True:
     print("다시 질문합니다.")
     question = input("무엇을 이야기하고 싶으세요?")
     print("말씀하신 내용:", question)
+    if "재미" in question:
+        print("재미를 느끼셨군요!")
+    elif "힘들" in question:
+        print("힘드셨군요. 잠시쉬어가도 괜찮아요.")
+    else:
+        print('그렇군요. 계속 이야기해 주세요.')
     answer = input("계속 할까요? (종료하려면 '종료' 입력):")
     if answer == "종료":
         break
