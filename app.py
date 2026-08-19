2b21fa9fe1eca8320ef174564f9a9b5e82b53369
myungwoohuh-alt <myungwoohuh@gmail.com>
2026-08-19 14:38:27 +0900

    중요기억 저장 및 삭제 기능 추가

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ response = client.responses.create(
 print(response.output_text)
 MAX_HISTORY = 1000
 HISTORY_FILE = "history.txt"
+MEMORY_FILE = "memory.txt"
+memory = ""
+if os.path.exists(MEMORY_FILE):
+    with open(MEMORY_FILE,"r", encoding="utf-8") as file:
+        memory=file.read()
 history=""
 if os.path.exists(HISTORY_FILE):
     with open(HISTORY_FILE,"r", encoding="utf-8") as file:
@@ -34,6 +39,18 @@ while True:
             file.write("")
         print("기억을 삭제했어요.")
         continue
+    elif question.startswith("기억해:"):
+        memory = question.replace("기억해:", "").strip()
+        with open(MEMORY_FILE,"a", encoding = "utf-8") as file:
+            file.write(memory + "\n")
+        print("중요한 기억으로 저장했어요.")
+        continue
+    elif question == "중요기억삭제":
+        memory = ""
+        with open(MEMORY_FILE,"w", encoding = "utf-8") as file:
+            file.write("")
+        print("중요한 기억을 삭제했어요.")
+        continue
     elif question in  ["도움", "도와줘", "도와주세요."]:
         print("어떤 도움이 필요한지 말씀해 주세요.")
         continue
@@ -45,7 +62,7 @@ while True:
         continue
     response = client.responses.create (
             model="gpt-5-mini",
-                input="사용자의 말에서 분위기를 파악하되 평가하거나 설명하지 말고 답변에 자연스럽게 반영하세요. 편안하고 자연스럽게 대화하되 항상 존댓말을 사용하세요.사용자의 말을 성급히 해석하거나 설명하려 하지 마세요.덧붙이는 조언.제안.질문 없이 필요한 말만 짧게 답하세요."+"\n이전대화:"+ history + "\n사용자:" + question 
+                input="사용자의 말에서 분위기를 파악하되 평가하거나 설명하지 말고 답변에 자연스럽게 반영하세요. 편안하고 자연스럽게 대화하되 항상 존댓말을 사용하세요.사용자의 말을 성급히 해석하거나 설명하려 하지 마세요.덧붙이는 조언.제안.질문 없이 필요한 말만 짧게 답하세요."+"\n이전대화:"+ history + "\n사용자:" + question + "\n중요기억:" + memory
                 ) 
     print(response.output_text)
     history = history + "\n사용자:" + question + "\nAI:" + response.output_text       
