e8db5341ecae37a29fb0549207f7913a46d5c736
myungwoohuh-alt <myungwoohuh@gmail.com>
2026-08-26 17:34:03 +0900

    문구 수정

--- cave.py
+++ cave.py
@@ -13,9 +13,9 @@ check_text=tk.Label(root,text="들어가기 전에, 준비물 한번 확인해 �
 check_text.place(x=30,y=30)
 computer = tk.Checkbutton(root,text="사용할 노트북이나 컴퓨터 있어요?")
 computer.place(x=30,y=60)
-internet = tk.Checkbutton(root,text="인터넷 연결되죠?")
+internet = tk.Checkbutton(root,text="Chrome 같은 것으로 인터넷 연결되죠?")
 internet.place(x=30,y=90)
-chatgpt = tk.Checkbutton(root,text="휴대폰으로 ChatGpt 사용할 수 있죠?")
+chatgpt = tk.Checkbutton(root,text="휴대폰으로 ChatGpt 같은 AI 사용할 수 있죠?")
 chatgpt.place(x=30,y=120)
 def enter_cave():
     global bg
@@ -29,15 +29,13 @@ def enter_cave():
     bg = ImageTk.PhotoImage(inside_image)
     label.config(image=bg)
 
-    tool_text = tk.Label(root,text="이 동굴과 만나려면 인터넷 연결이 필요해. \n\n" 
-    "그럼 인터넷에서 만나려면 어떤 도구가 필요할까?\n\n"
-    "그게 바로 브라우저야."
-    "아까 준비물 확인 때 Chrome 봤지?\n"
-    "Chrome이 브라우저 중 하나야.\n\n"
-    "여기서도 잘 연결되나 한 번 실행해 보자!",
+    tool_text = tk.Label(root,text="이 동굴과 만나려면 인터넷 연결이 필요해요. \n\n" 
+    "그럼 인터넷에서 만나려면 어떤 도구가 필요할까요?\n\n"
+    "그게 바로 Chrome 같은 브라우저입니다.\n\n"
+    "여기서도 잘 연결되나 한 번 실행해 볼까요?",
     font=("Arial", 12),
     justify="left")
     tool_text.place(x=30,y=30)
-ready=tk.Button(root,text="준비완료. 들어갑시다.",command=enter_cave)
+ready=tk.Button(root,text="준비 다 됐으면 우리 들어갈까요?",command=enter_cave)
 ready.place(x=30,y=160)
 root.mainloop()
